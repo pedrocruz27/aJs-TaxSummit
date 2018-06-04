1b838023bf9bb7d30de6ef0e321c1916cf933b3e
Pedro Cruz <pedro.galhano.cruz@novabase.pt>
2018-06-04 16:37:43 +0100

    teste com AngularTranslate

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
+var translationsEN = {
+	HEADLINE: 'What an awesome module!',
+	PARAGRAPH: 'Srsly!',
+	PASSED_AS_TEXT: 'Hey there! I\'m passed as text value!',
+	PASSED_AS_ATTRIBUTE: 'I\'m passed as attribute value, cool ha?',
+	PASSED_AS_INTERPOLATION: 'Beginners! I\'m interpolated!',
+	VARIABLE_REPLACEMENT: 'Hi {{name}}',
+	MISSING_TRANSLATION: 'Oops! I have not been translated into German...',
+	BUTTON_LANG_DE: 'German',
+	BUTTON_LANG_EN: 'English'
+  };
+  
+  var translationsDE= {
+	HEADLINE: 'Was für ein großartiges Modul!',
+	PARAGRAPH: 'Ernsthaft!',
+	PASSED_AS_TEXT: 'Hey! Ich wurde als text übergeben!',
+	PASSED_AS_ATTRIBUTE: 'Ich wurde als Attribut übergeben, cool oder?',
+	PASSED_AS_INTERPOLATION: 'Anfänger! Ich bin interpoliert!',
+	VARIABLE_REPLACEMENT: 'Hi {{name}}',
+	// MISSING_TRANSLATION is ... missing :)
+	BUTTON_LANG_DE: 'Deutsch',
+	BUTTON_LANG_EN: 'Englisch'
+  };
+	
 	// create the module and name it taxApp
-	var taxApp = angular.module('taxApp', ['ngRoute']);
+	var taxApp = angular.module('taxApp', ['ngRoute', 'pascalprecht.translate']);
 
 	// configure our routes 
 	taxApp.config(function($routeProvider) {
@@ -23,6 +47,16 @@
 				controller  : 'conferencesController'
 			})
 	});
+
+	//configure our translations
+	taxApp.config(['$translateProvider', function ($translateProvider) {
+		// add translation tables
+		$translateProvider.translations('en', translationsEN);
+		$translateProvider.translations('de', translationsDE);
+		$translateProvider.fallbackLanguage('en');
+		$translateProvider.preferredLanguage('en');
+	  }]);
+
 
 	taxApp.directive("headerDirective", function() {
 		return {
@@ -51,6 +85,15 @@
 	taxApp.controller('conferencesController', function($scope) {
 		$scope.message = 'Look! I am the conferences page.';
 	});
+
+
+
+	taxApp.controller('Ctrl', ['$translate', '$scope', function ($translate, $scope) {
+
+		$scope.changeLanguage = function (langKey) {
+		  $translate.use(langKey);
+		};
+	  }]);
 
 
 	
