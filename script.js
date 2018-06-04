a2740c7df144b88ba98abf2a3662506809645b99
Pedro Cruz <pedro.galhano.cruz@novabase.pt>
2018-06-04 17:48:23 +0100

    Translate ja se encontra funcional nno progresso atual

--- script.js
+++ script.js
@@ -1,25 +1,51 @@
 var translationsEN = {
-	HEADLINE: 'What an awesome module!',
-	PARAGRAPH: 'Srsly!',
-	PASSED_AS_TEXT: 'Hey there! I\'m passed as text value!',
-	PASSED_AS_ATTRIBUTE: 'I\'m passed as attribute value, cool ha?',
-	PASSED_AS_INTERPOLATION: 'Beginners! I\'m interpolated!',
-	VARIABLE_REPLACEMENT: 'Hi {{name}}',
-	MISSING_TRANSLATION: 'Oops! I have not been translated into German...',
-	BUTTON_LANG_DE: 'German',
-	BUTTON_LANG_EN: 'English'
+
+	//header
+	HEADER_CONFERENCES: "Conferences",
+	HEADER_ATENDEES: "Atendees",
+	HEADER_SPEAKERS: "Speakers",
+	HEADER_CALENDAR: "Calendar",
+	HEADER_HELP: "Help",
+
+	//home
+	HOME_JUMBO_SLOGAN: "The Challanges of a Digital World",
+	HOME_JUMBO_DATE: "Lisbon 23-25 October",
+	HOME_JUMBO_DAYS: "Days",
+	HOME_JUMBO_HOURS: "Hours",
+	HOME_JUMBO_MINUTES: "Minutes",
+
+	HOME_WHAT_IS: "What is the Tax Summit?",
+
+	HOME_DISCOVER_ACTIVITIES: "Take a look at our activities",
+	HOME_ACTIVITIES_TALKS: "Talks",
+	HOME_ACTIVITIES_PROGRAM: "Program",
+	HOME_ACTIVITIES_SPEAKERS: "Speakers"
+	
   };
   
-  var translationsDE= {
-	HEADLINE: 'Was für ein großartiges Modul!',
-	PARAGRAPH: 'Ernsthaft!',
-	PASSED_AS_TEXT: 'Hey! Ich wurde als text übergeben!',
-	PASSED_AS_ATTRIBUTE: 'Ich wurde als Attribut übergeben, cool oder?',
-	PASSED_AS_INTERPOLATION: 'Anfänger! Ich bin interpoliert!',
-	VARIABLE_REPLACEMENT: 'Hi {{name}}',
-	// MISSING_TRANSLATION is ... missing :)
-	BUTTON_LANG_DE: 'Deutsch',
-	BUTTON_LANG_EN: 'Englisch'
+  var translationsPT= {
+
+	//header
+	HEADER_CONFERENCES: "Conferências",
+	HEADER_ATENDEES: "Participantes",
+	HEADER_SPEAKERS: "Oradores",
+	HEADER_CALENDAR: "Calendário",
+	HEADER_HELP: "Ajuda",
+
+	//home
+	HOME_JUMBO_SLOGAN: "Os Desafios do Mundo Digital",
+	HOME_JUMBO_DATE: "Lisboa 23-25 Outubro",
+	HOME_JUMBO_DAYS: "Dias",
+	HOME_JUMBO_HOURS: "Horas",
+	HOME_JUMBO_MINUTES: "Minutos",
+
+	HOME_WHAT_IS: "O que é a Tax Summit?",
+
+	HOME_DISCOVER_ACTIVITIES: "Descobre as nossas atividades",
+	HOME_ACTIVITIES_TALKS: "Discursos",
+	HOME_ACTIVITIES_PROGRAM: "Programa",
+	HOME_ACTIVITIES_SPEAKERS: "Oradores"
+
   };
 	
 	// create the module and name it taxApp
@@ -52,7 +78,7 @@ var translationsEN = {
 	taxApp.config(['$translateProvider', function ($translateProvider) {
 		// add translation tables
 		$translateProvider.translations('en', translationsEN);
-		$translateProvider.translations('de', translationsDE);
+		$translateProvider.translations('pt', translationsPT);
 		$translateProvider.fallbackLanguage('en');
 		$translateProvider.preferredLanguage('en');
 	  }]);
@@ -87,13 +113,13 @@ var translationsEN = {
 	});
 
 
-
+	//changes language via the selected dropdown in the header
 	taxApp.controller('Ctrl', ['$translate', '$scope', function ($translate, $scope) {
  
 		$scope.languages = [
 			{name:'EN', value:'en'},
-			{name:'DE', value:'de'},
-		  ]
+			{name:'PT', value:'pt'},
+		];
 		  
 		$scope.selectedLanguage = 'en';
 
